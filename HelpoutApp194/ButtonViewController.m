0794ed3638a1a540b6e0a4480832e87d7b5b4799
Jennifer Goett <jen.goett@berkeley.edu>
2012-04-23 08:34:28 -0700

    OH MY GOD IT CAN SEND AUDIO TO A SERVER AND THE SERVER GETS IT RIGHT AND IT WORKS. This is really exciting. Now we just need to get Derrick's server to correctly process the audio, or possibly use my server instead?

--- HelpoutApp194/ButtonViewController.m
+++ HelpoutApp194/ButtonViewController.m
@@ -9,6 +9,7 @@
 #import "ButtonViewController.h"
 #import "SVProgressHUD.h"
 #import "HelpoutAppDelegate.h"
+#import "ASIFormDataRequest.h"
 
 @interface ButtonViewController ()
 
@@ -175,54 +176,56 @@
         [request setHTTPMethod:@"POST"];
         [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
         [request setHTTPBody:[[NSString stringWithFormat:@"&data=none"] dataUsingEncoding:NSUTF8StringEncoding]];    
-        
+        NSHTTPURLResponse *response;
+        NSData *urlData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
+        NSString *stringResponse = [[NSString alloc] initWithData:urlData encoding:NSASCIIStringEncoding]; 
+        NSLog(@"%@",stringResponse);
     }
     else {
         NSLog(@"They recorded a message");
+
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
         
-        //other way of doing this
-        [request setURL:[NSURL URLWithString:@"http://afternoon-moon-5773.heroku.com/audios/create_from_phone"]];
+        NSString* urlString = @"http://afternoon-moon-5773.heroku.com/audios/create_from_phone";
+        [request setURL:[NSURL URLWithString:urlString]];
         [request setHTTPMethod:@"POST"];
+        
+        NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+        NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+        [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+        
+        
         NSMutableData *body = [NSMutableData data];
-        [body appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"recording\"; filename=\"recording.caf\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        
+        //sound file
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithString:@"Content-Disposition: attachment; name=\"data\"; filename=\"sound.caf\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
         [body appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
-        [body appendData:[[NSString stringWithFormat:@"&username=%@&data=%@", user, [NSData dataWithContentsOfFile:soundFileURLPath]] dataUsingEncoding:NSUTF8StringEncoding]];
-        //[body appendData:];
+        [body appendData:[NSData dataWithContentsOfURL:audioRecorder.url]];
+        [body appendData:[[NSString stringWithString:@"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+
+        
+        //text
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"username\"\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithString: user] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithString: @"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        
+        //close the form
+        [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+          
         
         // set request body
         [request setHTTPBody:body];
-        NSLog(@"Request is %@", request);
-
         
+        // send the request (submit the form) and get the response
+        NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+        NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
         
-        /*ORIGINAL WAY OF DOING THIS
-        NSURL *url = [NSURL URLWithString:@"http://afternoon-moon-5773.heroku.com/audios/create_from_phone"];
-        [request setURL:url];
-        [request setHTTPMethod:@"POST"];
-        NSData *body = [NSData dataWithContentsOfFile:soundFileURLPath];
-       // NSString *body = @"Random text here";
-        NSLog(@"The file path (message) is %@", soundFileURLPath);
-        [request setValue:@"application/octet-stream" forHTTPHeaderField:@"Content-Type"];
-        [request setHTTPBody:[[NSString stringWithFormat:@"&username=%@&data=%@", user, body] dataUsingEncoding:NSUTF8StringEncoding]];
-         */
-    }
-
-    NSHTTPURLResponse *response;
-    NSData *urlData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
-    NSString *stringResponse = [[NSString alloc] initWithData:urlData encoding:NSASCIIStringEncoding]; 
-    NSLog(@"%@",stringResponse);
+        NSLog(@"%@", returnString);
+    }     
         
-    
-    //[response statusCode] == 200 && 
-    if (urlData != nil)
-    {
-        //it worked
-        [SVProgressHUD dismiss];
-    }
-    else  // something went wrong
-    {
-        [SVProgressHUD dismissWithError:@"Error"];
-    }
+    [SVProgressHUD dismiss];
 }
 
 
@@ -250,5 +253,6 @@
 {
     NSLog(@"Encode Error occurred");
 }*/
+
 
 @end
